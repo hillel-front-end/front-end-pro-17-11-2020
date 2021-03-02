a8b8b52aa427e8dabf08e812325aa67395d0bd78
Dmytro Shapovalenko <boozzd@gmail.com>
2021-03-02 23:44:12 +0200

    Lection 26 - NodeJS

---
+++ lections/lection_26_NodeJS_NPM/logger.js
@@ -0,0 +1,12 @@
+const url = 'http://mydomain.log';
+
+function log(message) {
+  // Send http request
+  console.log(url, message);
+}
+
+module.exports = log;
+// module.exports.log = log;
+// module.exports = {
+//   log: log,
+// };
